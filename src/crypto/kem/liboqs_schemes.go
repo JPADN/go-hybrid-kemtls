9ed08b676256bb1b3f93f4a88518bd90d5c65fb6
JPADN <jpadnalemao@gmail.com>
2022-01-24 16:20:10 -0300

    Renamed liboqsScheme struct to liboqsHybridScheme

--- src/crypto/kem/liboqs_schemes.go
+++ src/crypto/kem/liboqs_schemes.go
@@ -9,29 +9,29 @@ import (
 
 // Scheme for a Liboqs hybrid KEM.
 
-type liboqsScheme struct {
-	pqcName   string       // Passed as argument to oqs.KeyEncapsulation.Init()
-	classic   kem.Scheme
-	pqc 		  oqs.KeyEncapsulation
+type liboqsHybridScheme struct {
+	pqcName string // Passed as argument to oqs.KeyEncapsulation.Init()
+	classic kem.Scheme
+	pqc     oqs.KeyEncapsulation
 }
 
 // JP: TODO: Should I use a pointer?
 
 /* ---------------------------------- Kyber --------------------------------- */
 
-var p256_kyber512 liboqsScheme = liboqsScheme{
+var p256_kyber512 liboqsHybridScheme = liboqsHybridScheme{
 	"Kyber512",
 	hpke.KEM_P256_HKDF_SHA256.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p384_kyber768 liboqsScheme = liboqsScheme{
+var p384_kyber768 liboqsHybridScheme = liboqsHybridScheme{
 	"Kyber768",
 	hpke.KEM_P384_HKDF_SHA384.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p521_kyber1024 liboqsScheme = liboqsScheme{
+var p521_kyber1024 liboqsHybridScheme = liboqsHybridScheme{
 	"Kyber1024",
 	hpke.KEM_P521_HKDF_SHA512.Scheme(),
 	oqs.KeyEncapsulation{},
@@ -39,20 +39,19 @@ var p521_kyber1024 liboqsScheme = liboqsScheme{
 
 /* ---------------------------------- Saber --------------------------------- */
 
-var p256_lightsaber_kem liboqsScheme = liboqsScheme{
+var p256_lightsaber_kem liboqsHybridScheme = liboqsHybridScheme{
 	"LightSaber-KEM",
 	hpke.KEM_P256_HKDF_SHA256.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p384_saber_kem liboqsScheme = liboqsScheme{
+var p384_saber_kem liboqsHybridScheme = liboqsHybridScheme{
 	"Saber-KEM",
 	hpke.KEM_P384_HKDF_SHA384.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-
-var p521_firesaber_kem liboqsScheme = liboqsScheme{
+var p521_firesaber_kem liboqsHybridScheme = liboqsHybridScheme{
 	"FireSaber-KEM",
 	hpke.KEM_P521_HKDF_SHA512.Scheme(),
 	oqs.KeyEncapsulation{},
@@ -60,38 +59,37 @@ var p521_firesaber_kem liboqsScheme = liboqsScheme{
 
 /* ---------------------------------- NTRU ---------------------------------- */
 
-var p256_ntru_hps_2048_509 liboqsScheme = liboqsScheme{
+var p256_ntru_hps_2048_509 liboqsHybridScheme = liboqsHybridScheme{
 	"NTRU-HPS-2048-509",
 	hpke.KEM_P256_HKDF_SHA256.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p384_ntru_hps_2048_677 liboqsScheme = liboqsScheme{
+var p384_ntru_hps_2048_677 liboqsHybridScheme = liboqsHybridScheme{
 	"NTRU-HPS-2048-677",
 	hpke.KEM_P384_HKDF_SHA384.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-
-var p521_ntru_hps_4096_821 liboqsScheme = liboqsScheme{
+var p521_ntru_hps_4096_821 liboqsHybridScheme = liboqsHybridScheme{
 	"NTRU-HPS-4096-821",
 	hpke.KEM_P521_HKDF_SHA512.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p521_ntru_hps_4096_1229 liboqsScheme = liboqsScheme{
+var p521_ntru_hps_4096_1229 liboqsHybridScheme = liboqsHybridScheme{
 	"NTRU-HPS-4096-1229",
 	hpke.KEM_P521_HKDF_SHA512.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p384_ntru_hrss_701 liboqsScheme = liboqsScheme{
+var p384_ntru_hrss_701 liboqsHybridScheme = liboqsHybridScheme{
 	"NTRU-HRSS-701",
 	hpke.KEM_P384_HKDF_SHA384.Scheme(),
 	oqs.KeyEncapsulation{},
 }
 
-var p521_ntru_hrss_1373 liboqsScheme = liboqsScheme{
+var p521_ntru_hrss_1373 liboqsHybridScheme = liboqsHybridScheme{
 	"NTRU-HRSS-1373",
 	hpke.KEM_P521_HKDF_SHA512.Scheme(),
 	oqs.KeyEncapsulation{},
@@ -99,7 +97,7 @@ var p521_ntru_hrss_1373 liboqsScheme = liboqsScheme{
 
 /* ------------------------------------ . ----------------------------------- */
 
-var liboqsSchemeMap = map[ID]liboqsScheme{P256_Kyber512: p256_kyber512, P384_Kyber768: p384_kyber768, P521_Kyber1024: p521_kyber1024,
-																					P256_LightSaber_KEM: p256_lightsaber_kem, P384_Saber_KEM: p384_saber_kem, P521_FireSaber_KEM: p521_firesaber_kem,
-																					P256_NTRU_HPS_2048_509: p256_ntru_hps_2048_509, P384_NTRU_HPS_2048_677: p384_ntru_hps_2048_677, P521_NTRU_HPS_4096_821: p521_ntru_hps_4096_821, P521_NTRU_HPS_4096_1229: p521_ntru_hps_4096_1229,
-																					P384_NTRU_HRSS_701: p384_ntru_hrss_701, P521_NTRU_HRSS_1373: p521_ntru_hrss_1373}
+var liboqsSchemeMap = map[ID]liboqsHybridScheme{P256_Kyber512: p256_kyber512, P384_Kyber768: p384_kyber768, P521_Kyber1024: p521_kyber1024,
+	P256_LightSaber_KEM: p256_lightsaber_kem, P384_Saber_KEM: p384_saber_kem, P521_FireSaber_KEM: p521_firesaber_kem,
+	P256_NTRU_HPS_2048_509: p256_ntru_hps_2048_509, P384_NTRU_HPS_2048_677: p384_ntru_hps_2048_677, P521_NTRU_HPS_4096_821: p521_ntru_hps_4096_821, P521_NTRU_HPS_4096_1229: p521_ntru_hps_4096_1229,
+	P384_NTRU_HRSS_701: p384_ntru_hrss_701, P521_NTRU_HRSS_1373: p521_ntru_hrss_1373}
